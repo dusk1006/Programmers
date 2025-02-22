5747c999f919c196455c8dbf1821cfe1da3a2aed
dusk1006 <inyupj@gmail.com>
2025-02-23 03:21:02 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Long A = sc.nextLong();
+        Long B = sc.nextLong();
+        Long C = sc.nextLong();
+        System.out.println(A+B+C);
+        sc.close();
+    }
+}
